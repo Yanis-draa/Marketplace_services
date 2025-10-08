3ad9da67e8bc699336fbd1b399c82876d41d70c8
Yanis-draa <yanisdraa5gmail.com>
2025-10-08 19:55:15 +0100

    Modification

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,36 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Colors } from '../../constants/theme';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor:Colors.PRIMARY
+    }}>
         <Tabs.Screen name='home'
           options={{
             tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={24} color={color} />
+            ),
           }}
-        
-        
         />
         <Tabs.Screen name='explore'
           options={{
             tabBarLabel: 'Explore',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="search" size={24} color={color} />
+            ),
           }}
         />
         <Tabs.Screen name='profile'
           options={{
             tabBarLabel: 'Profile',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={24} color={color} />
+            ),
           }}
         />
     </Tabs>
