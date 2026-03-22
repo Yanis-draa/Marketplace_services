1fb0660cbd8f6b7cad0eff94b680a9fb016387ae
Yanis-draa <yanisdraa5gmail.com>
2026-03-23 00:37:12 +0100

    Derniers changements

--- app/business/add-business.tsx
+++ app/business/add-business.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Colors } from '@/constants/theme';
+import { useNavigation } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
 
 export default function AddBusiness() {
+
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Add New Business',
+      headerShown: true,
+    })
+  },[])
+
   return (
-    <View>
-      <Text>add-business</Text>
+    <View style={{padding: 20}}>
+      <Text style={{fontFamily: 'outfit-bold', fontSize: 25}}>Add New Business</Text>
+      <Text style={{fontFamily: 'outfit', color: Colors.GRAY}}>Fill all details in order to add new business </Text>
     </View>
   )
 }
--- components/Profile/MenuList.tsx
+++ components/Profile/MenuList.tsx
@@ -1,6 +1,8 @@
 import { Colors } from '@/constants/theme';
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+
 
 
 export default function MenuList() {
@@ -10,6 +12,7 @@ export default function MenuList() {
       id: 1,
       name: 'Add Business',
       icon: require('../../assets/images/add.png'),
+      path: '/business/add-business'
     },
     {
       id: 2,
@@ -27,31 +30,37 @@ export default function MenuList() {
       icon: require('../../assets/images/logout.png'), 
     },
   ];
+
+  const router = useRouter();
+  const onMenuClick = (item) => {
+    router.push(item.path)
+  }
   return (
     <View style={{ marginTop: 50 }}>
       <FlatList 
         data={menuItems}
         numColumns={2}
         renderItem={({ item, index }) => (
-          <View
-           style={{ 
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: 10, 
-            flex: 1,
-            padding: 10, 
-            borderWidth: 1,
-            backgroundColor: '#fff',
-            borderRadius: 15,
-            margin: 10,
-            borderColor: Colors.PRIMARY,
-          }}>
+          <TouchableOpacity
+            onPress={() => onMenuClick(item)}
+            style={{ 
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 10, 
+              flex: 1,
+              padding: 10, 
+              borderWidth: 1,
+              backgroundColor: '#fff',
+              borderRadius: 15,
+              margin: 10,
+              borderColor: Colors.PRIMARY,
+            }}>
             <Image
               source={item.icon}
               style={{ width: 50, height: 50 }}
             />
             <Text style={{fontFamily: 'outfit-medium', fontSize: 16, flex: 1}}>{item.name}</Text>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </View>
